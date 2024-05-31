cce2eabbc0fcc1a16b72f989bdb9d1f8fd5e1371
TheRatatoskr <joseph.ewert@gmail.com>
2024-05-31 17:31:23 -0500

    reworked jumping a bit

--- Assets/Scripts/Player/ActionMovement.cs
+++ Assets/Scripts/Player/ActionMovement.cs
@@ -11,9 +11,22 @@ public class ActionMovement : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject model;
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Transform wallRaySourceFeet;
+    [SerializeField] private Transform wallRaySourceBody;
+    [SerializeField] private Transform wallRaySourceHead;
+
+    private bool footHitWall;
+    private bool bodyHitWall;
+    private bool headHitWall;
+
+    [SerializeField] private float wallCheckLength;
+    [SerializeField] private float stopIfWallIsThisClose = .13f;
+
+    private int isLeft = 1;
 
     private Vector2 currentMovement;
 
+    private float wallAdjustment = 1;
     private bool isAlive = true;
 
     private void Start()
@@ -22,8 +35,21 @@ public class ActionMovement : MonoBehaviour
     }
     private void Update()
     {
-        if(Time.timeScale <= .9f) return; 
-        transform.Translate(currentMovement.x * moveSpeed * Time.deltaTime, 0, 0);
+        WallCheckerFoot();
+        WallCheckerBody();
+        WallCheckerHead();
+
+        if (headHitWall || footHitWall || bodyHitWall)
+        {
+            wallAdjustment = 0;
+        }
+        else
+        {
+            wallAdjustment = 1;
+        }
+        
+        if (Time.timeScale <= .9f) return; 
+        transform.Translate(currentMovement.x * moveSpeed * Time.deltaTime * wallAdjustment, 0, 0);
         animator.SetFloat("forwardSpeed", Math.Abs(currentMovement.x));
 
         //position validation checks
@@ -31,18 +57,102 @@ public class ActionMovement : MonoBehaviour
         transform.rotation = Quaternion.identity;
 
     }
+
+    private void WallCheckerFoot()
+    {
+        Ray ray = new Ray(wallRaySourceFeet.position, Vector3.right * isLeft);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, wallCheckLength))
+        {
+            if(hitInfo.distance <= stopIfWallIsThisClose)
+            {
+                Debug.Log("I hit the wall");
+                footHitWall = true;
+            }
+            else
+            {
+                Debug.Log("No walls here!");
+                footHitWall = false;
+            }
+        }
+        else
+        {
+            Debug.Log("nothing found");
+            footHitWall = false;
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * wallCheckLength, Color.magenta);
+
+    }
+
+    private void WallCheckerBody()
+    {
+        Ray ray = new Ray(wallRaySourceBody.position, Vector3.right * isLeft);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, wallCheckLength))
+        {
+            if (hitInfo.distance <= stopIfWallIsThisClose)
+            {
+                Debug.Log("I hit the wall");
+                bodyHitWall = true;
+            }
+            else
+            {
+                Debug.Log("No walls here!");
+                bodyHitWall= false;
+            }
+
+        }
+        else
+        {
+            Debug.Log("nothing found");
+            bodyHitWall = false;
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * wallCheckLength, Color.magenta);
+
+    }
+
+    private void WallCheckerHead()
+    {
+        Ray ray = new Ray(wallRaySourceHead.position, Vector3.right * isLeft);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, wallCheckLength))
+        {
+            if (hitInfo.distance <= stopIfWallIsThisClose)
+            {
+                Debug.Log("I hit the wall");
+                headHitWall = true;
+            }
+            else
+            {
+                Debug.Log("No walls here!");
+                headHitWall= false;
+            }
+
+        }
+        else
+        {
+            Debug.Log("nothing found");
+            headHitWall = false;
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * wallCheckLength, Color.magenta);
+
+    }
+
     private void CalculateMovement(Vector2 joyStick)
     {
 
         currentMovement = joyStick;
         if(currentMovement.x > 0)
         {
-            //sleepy joe says better way is totes possible
+            isLeft = 1;
             model.transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 90f, 0);
         } 
         else if (currentMovement.x < 0)
         {
-            //sleepy joe says better way is totes possible
+            isLeft = -1;
             model.transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + -90f, 0);
         }
     }
